0a2bdd4f34c11c09980becfb5b83dd07e73aaa83
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-26 12:12:43 -0500

    Deploy configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'apprenticeradar2',
+    appId: '1:267782581994:web:8dc6edacb8a984d9e63193',
+    storageBucket: 'apprenticeradar2.appspot.com',
+    apiKey: 'AIzaSyDwKwc3iLXJK4Yanbf-vPwuMXTozfSvz6k',
+    authDomain: 'apprenticeradar2.firebaseapp.com',
+    messagingSenderId: '267782581994',
+  },};
