65958f24f88f0db2df8465dbe2d61c746fd4633e
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-26 12:13:16 -0500

    Routes configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { RadarPageComponent } from './Components/Pages/radar-page/radar-page.component';
 import { RadarComponent } from './Components/Pure/radar/radar.component';
 import { NewRadarPageComponent } from './Components/Pages/new-radar-page/new-radar-page.component';
+import { MainPageComponent } from './Components/Pages/main-page/main-page.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: MainPageComponent,
+  },
   {
     path: 'radar',
     component: RadarPageComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RadarComponent } from './Components/Pure/radar/radar.component';
 import { RadarListComponent } from './Components/Container/radar-list/radar-list.component';
 import { RadarPageComponent } from './Components/Pages/radar-page/radar-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -15,6 +16,13 @@ import { MatCardModule } from '@angular/material/card';
 import { NewRadarPageComponent } from './Components/Pages/new-radar-page/new-radar-page.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MainPageComponent } from './Components/Pages/main-page/main-page.component';
+import { LoginComponent } from './Components/Pure/login/login.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -24,17 +32,25 @@ import { MatInputModule } from '@angular/material/input';
     RadarPageComponent,
     NavBarComponent,
     NewRadarPageComponent,
+    MainPageComponent,
+    LoginComponent,
   ],
   imports: [
     MatFormFieldModule,
     MatInputModule,
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatExpansionModule,
     MatGridListModule,
     MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
   bootstrap: [AppComponent],
