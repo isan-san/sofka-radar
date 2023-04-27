e05cbb8c7ff77ad1896ea53270eaee40930c072c
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-26 22:58:12 -0500

    Create the form that allows to reate a new user

---
+++ src/app/Components/Pure/form-user/form-user.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
+
+@Component({
+  selector: 'app-form-user',
+  templateUrl: './form-user.component.html',
+  styleUrls: ['./form-user.component.scss'],
+})
+export class FormUserComponent {
+  userForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    mail: new FormControl(''),
+    role: new FormControl('')
+  });
+  role: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSave() {
+    this.authService.register({email: this.userForm.value.mail, password: '123456', role: this.userForm.value.role});
+    this.router.navigate(['/users'])
+  }
+}
